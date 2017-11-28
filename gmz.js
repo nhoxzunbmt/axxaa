ab27765d034985d716eb8b21994ca8798529fc52
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2017-11-28 18:03:05 +0700

    UPDATY

--- gmz.js
+++ gmz.js
@@ -4,10 +4,10 @@ var fs = require('fs')
 // resize and remove EXIF profile data
 gm('z.jpg')
     //.resize(240, 240)
-    .font("timesbd.ttf", 32)
-    .drawText('text_data.txt')
-    .noProfile()
+    .font("arialbd.ttf", 32)
+    .drawText(100,100,'Tràn ợ thanh Á')
+    .encoding('Unicode')
     .write('resize.png', function (err) {
-        if (!err) console.log('done');
+        //if (!err) console.log('done');
         console.log(err);
     });
