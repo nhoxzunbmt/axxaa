9c1f80d1203315462721c7ade065f161e5d0e4d6
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2017-11-30 00:40:02 +0700

    Update

--- start.js
+++ start.js
@@ -3,57 +3,87 @@ var utf8 = require('utf8');
 var path = require('path');
 var dir = require('node-dir');
 var videoshow = require('videoshow');
+var lodash = require('lodash');
+var Promise = require('promise');
+var Q = require("q");
+var ffmpeg = require('fluent-ffmpeg');
+var ffmpeg_install = require('@ffmpeg-installer/ffmpeg');
+var ffprobe_install = require('@ffprobe-installer/ffprobe');
+var colors = require('colors');
 
-var ffmpeg = require('@ffmpeg-installer/ffmpeg');
-var ffprobe = require('@ffprobe-installer/ffprobe');
-
-videoshow.ffmpeg.setFfmpegPath(ffmpeg.path);
-videoshow.ffmpeg.setFfprobePath(ffprobe.path);
+videoshow.ffmpeg.setFfmpegPath(ffmpeg_install.path);
+videoshow.ffmpeg.setFfprobePath(ffprobe_install.path);
 
 const IMAGE_ROOT = __dirname + '\\input\\images';
 const AUDIO_ROOT = __dirname + '\\input\\audio';
 const FONT_ROOT = __dirname + '\\input\\fonts';
 
 const bg_image = IMAGE_ROOT + '\\bg\\bg.png';
+const font_text = FONT_ROOT + "/arialbd.ttf";
+const images_folder = IMAGE_ROOT + '\\animals';
+const images_render = __dirname + '\\merge';
+const images_resize = __dirname + '\\resize';
+const video_temp = __dirname + '\\video_temp';
 
 
-var images_folder = IMAGE_ROOT + '\\animals';
-
-var s = 'Anh em nghĩ chiếc YouTube Phone sẽ như thế nào?\n';
+var s = 'Anh em nghĩ chiếc YouTube Phone sẽ như thế nào?';
 s = utf8.encode(s);
 
 
-
-dir.promiseFiles(images_folder)
-    .then(function (files) {
-        files.forEach(function (file) {
-            var file_name = path.basename(file);
-            console.log('-----------------------');
-            console.log(file_name);
-            hello_file(file_name);
-            resize(file);
-        })
-    })
-    .then(function () {
-
-        var images = [
-            {
-                path: __dirname + '/result_merge_bg_resize_animals_hero_fennec2.jpg'
-            }, {
-                path: __dirname + '/result_merge_bg_resize_animals_hero_panda.jpg',
-                loop: 5
-            }
-        ];
-        //make_video(images);
-    })
-    .catch(function (e) {
-        console.log(e);
-    });
-
-function hello_file(file) {
-    console.log('This is ' + file);
+function log(str) {
+    str = '=================================' + str + '====================================';
+    console.log(str.inverse);
 }
 
+var promise = Q.fcall(function () {
+    return log('THIS IS TOOL MAKE VIDEO');
+});
+
+promise
+    .then(function (data) {
+        resize_img();
+        return 'resize_img success';
+    })
+    .then(function (data) {
+        merge_bg();
+        return 'merge_bg success';
+    })
+
+    .then(function (data) {
+        make_small_video();
+    })
+    .then(function (data) {
+        merge_videos();
+    })
+;
+
+
+function resize_img() {
+    log('resize_img');
+    dir.promiseFiles(images_folder)
+        .then(function (files) {
+            files.forEach(function (file) {
+                resize(file);
+            })
+        })
+        .catch(function (e) {
+            console.log(e);
+        });
+
+}
+
+function make_small_video() {
+    log('make_small_video');
+    dir.promiseFiles(images_render)
+        .then(function (files) {
+            files.forEach(function (file) {
+                make_video(file);
+            })
+        })
+        .catch(function (e) {
+            console.log(e);
+        });
+}
 
 function resize(file) {
     var base_name = path.basename(file);
@@ -61,28 +91,41 @@ function resize(file) {
     gm(file).resize(440, 320).write(file_name, function (err) {
         if (err) console.log(err);
         console.log('Done resize!');
-        //merge_bg(file_name);
     });
 }
 
 
-function merge_bg(file) {
-    var base_name = path.basename(file);
-    var file_name = 'merge_bg_' + base_name;
-    gm('bg.png')
-        .composite(file)
-        .geometry('+136+180')
-        .write(file_name, function (err) {
-            if (err) console.log(err);
-            console.log('Done merge_bg!');
-            add_text(file_name);
+function merge_bg() {
+    log('merge_bg');
+
+    dir.promiseFiles(images_resize)
+        .then(function (files) {
+            files.forEach(function (file) {
+                var base_name = path.basename(file);
+                var file_name = 'merge/' + base_name;
+
+                gm(bg_image)
+                    .composite(file)
+                    .geometry('+136+180')
+                    .write(file_name, function (err) {
+                        if (err) console.log(err);
+                        console.log('Done merge_bg!');
+                        add_text(file_name);
+                    });
+            })
+        })
+        .catch(function (e) {
+            console.log(e);
         });
+
+
 }
 
 function add_text(file) {
-    var file_name = 'result_' + path.basename(file);
+    log('add_text');
+    var file_name = 'merge/' + path.basename(file);
     gm(file)
-        .font("arialbd.ttf", 12)
+        .font(font_text, 36)
         .drawText(780, 220, s)
         .drawText(800, 450, s)
         .encoding('Unicode')
@@ -93,25 +136,31 @@ function add_text(file) {
 }
 
 
-function make_video(images) {
-    var audio = __dirname + '/song.mp3';
+function make_video(file) {
+    log('make_video : ' + file);
+
+    var paths = path.parse(file);
+    var video_tmp = video_temp + '\\' + paths.name + '.mp4';
+
+    var audio = AUDIO_ROOT + '/song.mp3';
     var audioParams = {
         fade: true,
         delay: 2 // seconds
     };
     var videoOptions = {
-        fps: 25,
+        fps: 30,
         loop: 5, // seconds
         transition: true,
         transitionDuration: 1, // seconds
-        videoBitrate: 1024,
+        videoBitrate: 1080,
         videoCodec: 'libx264',
-        size: '640x?',
+        size: '960x?',
         audioBitrate: '128k',
         audioChannels: 2,
         format: 'mp4',
         pixelFormat: 'yuv420p'
     };
+
     var logo = __dirname + '/logo.png';
     var logoParams = {
         start: 5,
@@ -119,32 +168,53 @@ function make_video(images) {
         xAxis: 20,
         yAxis: 20
     };
-    images.forEach(function (image) {
 
-        videoshow(image, videoOptions)
-            .audio(audio, audioParams)
-            //.logo(logo,logoParams)
-            .save('video.mp4')
-            .on('start', function (command) {
-                console.log('ffmpeg process started:', command)
-            })
-            .on('error', function (err, stdout, stderr) {
-                console.error('Error:', err)
-                console.error('ffmpeg stderr:', stderr)
-            })
-            .on('end', function (output) {
-                console.error('Video created in:', output)
-            })
-
-    });
-
+    videoshow([file], videoOptions)
+        .audio(audio, audioParams)
+        //.logo(logo,logoParams)
+        .save(video_tmp)
+        .on('start', function (command) {
+            //console.log('ffmpeg process started:', command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            console.error('Error:', err)
+            console.error('ffmpeg stderr:', stderr)
+        })
+        .on('end', function (output) {
+            console.error('Video created in:', output)
+        })
 }
 
 
 function merge_videos() {
-    ffmpeg()
-        .mergeAdd('/path/to/input1.avi')
-        .mergeAdd('/path/to/input2.avi');
+    log('merge_videos');
+    dir.promiseFiles(video_temp)
+        .then(function (files) {
+
+            var p = Q.fcall(function () {
+                var ff = ffmpeg();
+                files.forEach(function (f) {
+                    ff.input(f);
+                });
+                return ff;
+            });
+
+            p.then(function (ff) {
+                ff.on('end', function () {
+                    console.log('files have been merged succesfully');
+                })
+                    .on('error', function (err) {
+                        console.log('an error happened: ' + err.message);
+                    })
+                    .mergeToFile('video_result/video.mp4');
+
+            });
+        })
+
+        .catch(function (e) {
+            console.log(e);
+        });
+
 }
 
 
